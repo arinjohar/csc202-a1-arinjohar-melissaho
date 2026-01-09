47ca1a496243350084b02f78380177cf8228479f
melissakho <mho63@calpoly.edu>
2026-01-09 13:08:39 -0800

    commit

--- ghg.py
+++ ghg.py
@@ -7,6 +7,25 @@ sys.setrecursionlimit(10**6)
 
 # Your data definitions and functions go here.
 
+calpoly_email_addresses = ["mho63@calpoly.edu", "arjohar@calpoly.edu"]
+
+@dataclass(frozen=True)
+class GlobeRect:
+    lo_lat : float
+    hi_lat : float
+    west_long : float
+    east_long : float
+
+@dataclass(frozen=True)
+class Region: 
+    rect : GlobeRect
+    name : str
+    terrain : TypeAlias = Literal['ocean', 'mountain', 'forest', 'other']
+
+@dataclass(frozen=True)
+class 
+
+
 class Tests(unittest.TestCase):
     # Put your test cases in here.
     pass
